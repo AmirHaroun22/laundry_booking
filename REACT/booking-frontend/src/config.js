f257ca6a5b065a95a5c05a18356a3791850971d5
amir <amir.haroun@rwth-aachen.de>
2025-06-24 14:03:05 +0200

    Refine API_BASE_URL in config

--- REACT/booking-frontend/src/config.js
+++ REACT/booking-frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL   || 'http://api.mydomain.com/api',
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
 };
 
 export default config;
